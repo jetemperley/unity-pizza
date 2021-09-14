59f337751da6d899f3e4cd05e7901a105d26d1a2
jetemperley <57704985+jetemperley@users.noreply.github.com>
2021-09-14 16:06:39 +1000

    Car setup

---
+++ Assets/Scripts/Look.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour {
+
+    float x, y;
+    public float sensitivity = 5;
+
+    void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+        x = transform.localRotation.eulerAngles.y;
+        y = transform.localRotation.eulerAngles.x;
+
+    }
+
+    void Update () {
+
+            x += Input.GetAxis ("Mouse X") * sensitivity;
+            y += Input.GetAxis ("Mouse Y") * sensitivity;
+
+
+
+            Quaternion angles = Quaternion.Euler(0, x, 0) * Quaternion.Euler(-y, 0, 0);
+            transform.localRotation = angles;
+
+            
+            // if (angles.x-y > 270 || angles.x-y < 90){
+            //     camera.Rotate(-y, 0, 0, Space.Self);    
+            // }
+
+
+    }
+
+}
